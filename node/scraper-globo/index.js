4e6a590747c6ad3c507be96cd2aa0b17e664d29c
wellington moreira <wsantos08@hotmail.com>
2024-12-27 10:05:21 -0300

    redirecionados estudos de regressao linear para repositorio data-science

--- node/scraper-globo/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app');
-
-const port = 3000;
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running at http://localhost:${port}/posts`);
-});
--- node/scraper-globo/src/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const postsRoute = require('./routes/posts');
-
-const app = express();
-
-app.use(postsRoute);
-
-module.exports = app;
