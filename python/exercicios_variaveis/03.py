e599d5164d858db9d4dbc958c28b68cfc9fdc095
wellington moreira <wsantos08@hotmail.com>
2025-01-03 09:24:12 -0300

    organizando diretorios

--- python/exercicios_variaveis/03.py
+++
@@ -1,8 +0,0 @@
-"""
-Receba um número do usuário como string, converta para inteiro e 
-realize uma operação matemática.
-"""
-
-num = int(input("Digite o ano de seu nascimento: "))
-
-print(f'Você têm(faz) {2025 - num} anos')
--- python/exercicios_variaveis/04.py
+++
@@ -1,8 +0,0 @@
-"""
-Receba duas palavras do usuário e una-as em uma única frase.
-"""
-
-nome = input('Primeiro nome: ')
-sobrenome = input('Sobrenome: ')
-
-print(f'{nome} {sobrenome}'[::-1])
