b676e54b977f9eceec18001fbb8985d659c11c9d
wellington moreira <wsantos08@hotmail.com>
2025-01-30 22:20:51 -0300

    revisando pratica de css modules react

--- react/fundamentos-reactjs/src/App.jsx
+++ react/fundamentos-reactjs/src/App.jsx
@@ -1,23 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
-    <Header />
-      <h1>Posts</h1>
-      <Post 
-        author="escova"
-        title="Titulo 1"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptas accusantium porro quidem quia. Rem quia perspiciatis perferendis facilis suscipit."
-      />
-      <Post 
-        author="escova"
-        title="Titulo 2"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptas accusantium porro quidem quia. Rem quia perspiciatis perferendis facilis suscipit."
-      />
-      <Post />
+      <Header />
+        <div className={styles.wrapper}>
+          < Sidebar />  
+          <main>
+            <Post 
+              author="escova"
+              title="Titulo 1"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptas accusantium porro quidem quia. Rem quia perspiciatis perferendis facilis suscipit."
+            />
+            <Post 
+              author="escova"
+              title="Titulo 2"
+              content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptas accusantium porro quidem quia. Rem quia perspiciatis perferendis facilis suscipit."
+            />
+            <Post />
+          </main>
+      </div>
     </>
   )
 }
