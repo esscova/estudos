b9a934201f934bbdcb044fbd5eb2755f4d04c7a0
wellington moreira <wsantos08@hotmail.com>
2025-02-08 21:55:46 -0300

    SQL

--- SQL/data/queries/ecommerce/tratamento-de-dados.sql
+++ SQL/data/queries/ecommerce/tratamento-de-dados.sql
@@ -21,3 +21,23 @@ SELECT REPLACE(112122::VARCHAR,'1','A')
 -- select '2021-10-01' - '2021-02-01'
 
 SELECT CAST('2021-10-01' AS DATE) - CAST('2021-02-01' AS DATE)
+
+-- Agrupamento de dados com CASE WHEN
+-- Calcule o nº de clientes que ganham abaixo de 5k, entre 5k e 10k, entre 10k e 15k e acima de 15k
+
+WITH faixa_de_renda AS (
+	SELECT 
+		income,
+		CASE
+			WHEN income < 5000 THEN '0-500'
+			WHEN income >= 5000 AND income <10_000 THEN '5000-10000'
+			WHEN income > 10_000 AND income < 15_000 THEN '10000-15000'
+			ELSE '15000+'
+			END AS faixa_renda
+	FROM sales.customers
+)
+SELECT 
+	faixa_renda, 
+	COUNT(*)
+FROM faixa_de_renda
+GROUP BY faixa_renda;
