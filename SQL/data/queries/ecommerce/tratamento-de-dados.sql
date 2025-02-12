242dc2362d05f2b21e8ec7eea728db54b9d207c4
wellington moreira <wsantos08@hotmail.com>
2025-02-12 10:02:36 -0300

    sql para tratamento de dados

--- SQL/data/queries/ecommerce/tratamento-de-dados.sql
+++ SQL/data/queries/ecommerce/tratamento-de-dados.sql
@@ -53,3 +53,23 @@ SELECT
 		FROM temp_tables.regions
 	)) AS populacao_ajustada
 FROM temp_tables.regions;
+
+
+-- (Exemplo 1) Corrija o primeiro elemento das queries abaixo utilizando os comandos 
+-- de tratamento de texto para que o resultado seja sempre TRUE 
+
+-- select 'São Paulo' = 'SÃO PAULO'
+select upper('São Paulo') = 'SÃO PAULO'
+
+
+-- select 'São Paulo' = 'são paulo'
+select lower('São Paulo') = 'são paulo'
+
+
+-- select 'SÃO PAULO     ' = 'SÃO PAULO'
+select trim('SÃO PAULO     ') = 'SÃO PAULO'
+
+
+-- select 'SAO PAULO' = 'SÃO PAULO'
+select replace('SAO PAULO', 'SAO', 'SÃO') = 'SÃO PAULO'
+
