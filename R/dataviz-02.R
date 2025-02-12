a6d444d7d57adef41e9660817ebf2d02d2cbfdac
wellington moreira <wsantos08@hotmail.com>
2025-02-12 08:41:25 -0300

    dataviz com R

---
+++ R/dataviz-02.R
@@ -0,0 +1,10 @@
+# Análise de Séries Temporais
+# Carregue o dataset `AirPassengers` e plote a série temporal.
+
+# dados
+data("AirPassengers")
+AirPassengers
+
+# plot
+plot(AirPassengers, main = "Número de Passageiros por Mês", xlab = "Tempo", ylab = "Passageiros")
+
