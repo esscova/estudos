e00b877e64b373c2e48dc2307d290e056885e5c3
wellington moreira <wsantos08@hotmail.com>
2025-02-25 08:01:49 -0300

    SQL criando e populando tabelas

--- SQL/data/queries/ecommerce/tabelas.sql
+++ SQL/data/queries/ecommerce/tabelas.sql
@@ -9,3 +9,33 @@ INTO temp_tables.customers_age
 FROM sales.customers;
 
 SELECT * FROM temp_tables.customers_age;
+
+-- Criação de tabela a partir do zero
+-- Crie uma tabela com a tradução dos status profissionais dos clientes. 
+-- Chame-a de temp_tables.profissoes
+
+-- selecao dos dados
+SELECT DISTINCT professional_status
+FROM sales.customers;
+
+-- criando tabela
+CREATE TABLE temp_tables.profissoes(
+	professional_status VARCHAR,
+	status_profissional VARCHAR
+);
+
+-- populando
+INSERT INTO temp_tables.profissoes(professional_status, status_profissional)
+VALUES
+	('freelancer', 'freelancer'),
+	('retired', 'aposentado(a)'),
+	('clt', 'clt'),
+	('self_employed', 'autônomo(a)'),
+	('other', 'outro'),
+	('businessman', 'empresário(a)'),
+	('civil_servant', 'funcionário público(a)'),
+	('student', 'estudante');
+
+-- consultando dados
+SELECT *
+FROM temp_tables.profissoes;
