710a1318ff9d1f9cc88f908be8f883375d28257f
wellington moreira <wsantos08@hotmail.com>
2025-03-13 05:23:31 -0300

    query para dashboard

--- SQL/data/queries/ecommerce/dashboard.sql
+++ SQL/data/queries/ecommerce/dashboard.sql
@@ -52,3 +52,20 @@ where paid_date between '2021-08-01' and '2021-08-31'
 group by pais,  estado
 order by "vendas (#)" desc
 limit 5
+
+-- (Query 3) Marcas que mais venderam no mês
+-- Colunas: marca, vendas (#)
+
+select 
+	pro.brand as marca,
+	count(fun.paid_date) as "vendas (#)"
+
+from sales.funnel as fun
+left join sales.products as pro
+	on fun.product_id = pro.product_id
+
+where paid_date between '2021-08-01' and '2021-08-31'
+
+group by marca
+order by "vendas (#)" desc
+limit 5
