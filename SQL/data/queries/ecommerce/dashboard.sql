b0b1bc2420cf22b36bb70bf80ad40e55de6d0d32
wellington moreira <wsantos08@hotmail.com>
2025-03-14 06:08:38 -0300

    query para dashboard

--- SQL/data/queries/ecommerce/dashboard.sql
+++ SQL/data/queries/ecommerce/dashboard.sql
@@ -69,3 +69,18 @@ where paid_date between '2021-08-01' and '2021-08-31'
 group by marca
 order by "vendas (#)" desc
 limit 5
+
+-- (Query 4) Lojas que mais venderam
+-- Colunas: loja, vendas (#)
+
+select 
+	sto.store_name as loja,
+	count(fun.paid_date) as "vendas (#)"
+
+from sales.funnel as fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+where paid_date between '2021-08-01' and '2021-08-31'
+group by loja
+order by "vendas (#)" desc
+limit 5
