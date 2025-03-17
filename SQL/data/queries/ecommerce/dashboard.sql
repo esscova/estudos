5018655372962f03f0f91b0b0bf1e18fc1e1d256
wellington moreira <wsantos08@hotmail.com>
2025-03-17 10:22:08 -0300

    queries para dashboard

--- SQL/data/queries/ecommerce/dashboard.sql
+++ SQL/data/queries/ecommerce/dashboard.sql
@@ -84,3 +84,24 @@ where paid_date between '2021-08-01' and '2021-08-31'
 group by loja
 order by "vendas (#)" desc
 limit 5
+
+-- (Query 5) Dias da semana com maior número de visitas ao site
+-- Colunas: dia_semana, dia da semana, visitas (#)
+
+select
+	extract('dow' from visit_page_date) as dia_semana,
+	case
+		when extract('dow' from visit_page_date)=0 then 'domingo'
+		when extract('dow' from visit_page_date)=1 then 'segunda'
+		when extract('dow' from visit_page_date)=2 then 'terça'
+		when extract('dow' from visit_page_date)=3 then 'quarta'
+		when extract('dow' from visit_page_date)=4 then 'quinta'
+		when extract('dow' from visit_page_date)=5 then 'sexta'
+		when extract('dow' from visit_page_date)=6 then 'sábado'
+		else null end as "dia da semana",
+	count(*) as "visitas (#)"
+
+from sales.funnel
+where visit_page_date between '2021-08-01' and '2021-08-31'
+group by dia_semana
+order by dia_semana
